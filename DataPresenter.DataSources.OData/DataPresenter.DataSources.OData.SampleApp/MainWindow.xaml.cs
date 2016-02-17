2443d78b74e117ebb5549d2b926407b7d85da4a2
JM <JM@infragistics.com>
2016-02-17 15:52:55 -0500

    Refactored much of the sample app logic into a SampleViewModel class.
    Added support to the sample app for displaying a summary of the currently selected data source including RecordCount.
    
    Removed some extraneous options from the sample app.

--- DataPresenter.DataSources.OData/DataPresenter.DataSources.OData.SampleApp/MainWindow.xaml.cs
+++ DataPresenter.DataSources.OData/DataPresenter.DataSources.OData.SampleApp/MainWindow.xaml.cs
@@ -1,146 +1,39 @@
-﻿using DataPresenter.DataSources.OData;
-using Infragistics.Controls.Interactions;
+﻿using Infragistics.Controls.Interactions;
 using Infragistics.Windows.Controls;
 using Infragistics.Windows.DataPresenter;
-using Infragistics.Windows.Themes;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DataPresenter.DataSources.OData.SampleApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
     {
-		#region Member Variables
-		private Infragistics.Windows.DataPresenter.GridView			_gridView;
-		private Infragistics.Windows.DataPresenter.CardView			_cardView;
-		private Infragistics.Windows.DataPresenter.CarouselView		_carouselView;
-		private Infragistics.Windows.DataPresenter.TreeView			_treeView;
-		#endregion //Member Variables
-
 		#region Constructor
 		public MainWindow()
         {
             InitializeComponent();
 
-			this.numPageSizeRequested.Value						= 200;
-			this.numMaxCachedPages.Value						= 200;
-			this.cboOdataSources.SelectedIndex					= 0;
-			this.cboDataPresenterView.SelectedIndex				= 0;
-			this.cboRecordFilterLogicalOperator.SelectedIndex	= 0;
-
+			#region UI Setup
 			// Listen to the XamDataPresenter's ThemeChanged event so we can initialize the DataPendingOverlayBrush color picker when the Theme changes.
 			this.dataPresenter1.ThemeChanged += (s,e) => { this.Dispatcher.BeginInvoke(new Action(() => this.InitializeColorPicker()), System.Windows.Threading.DispatcherPriority.ApplicationIdle); };
 
 			// Initialize the list of themes and select 'Office2013'.
 			this.cboThemes.ItemsSource		= Infragistics.Windows.Themes.ThemeManager.GetThemes();
-			this.cboThemes.SelectedValue	= "Office2013";
+			this.cboThemes.SelectedValue	= "RoyalDark";
 			
 			// Initialize the list of XamBusyIndicator Animations.
 			this.cboBusyIndicatorAnimations.ItemsSource		= typeof(BusyAnimations).GetFields(BindingFlags.Public | BindingFlags.Static).Select((prop) => prop.Name).ToArray();
 			this.cboBusyIndicatorAnimations.SelectedValue	= "Gears";
+			#endregion //UI Setup
 		}
 		#endregion //Constructor
-
-		#region Private Properties
-
-		#region CardView
-		private Infragistics.Windows.DataPresenter.CardView CardView
-		{
-			get
-			{
-				if (null == this._cardView)
-					this._cardView = new Infragistics.Windows.DataPresenter.CardView();
-
-				return this._cardView;
-			}
-		}
-		#endregion //CardView
-
-		#region CarouselView
-		private Infragistics.Windows.DataPresenter.CarouselView CarouselView
-		{
-			get
-			{
-				if (null == this._carouselView)
-					this._carouselView = new Infragistics.Windows.DataPresenter.CarouselView();
-
-				return this._carouselView;
-			}
-		}
-		#endregion //CarouselView
-
-		#region CurrentDataSource
-		private ODataDataSource CurrentDataSource
-		{
-			get
-			{
-				if (null != this.dataPresenter1)
-					return this.dataPresenter1.DataSource as ODataDataSource;
-
-				return null;
-			}
-		}
-		#endregion //CurrentDataSource
-
-		#region PageSizeRequested
-		private int PageSizeRequested
-		{
-			get { return Convert.ToInt32((double)this.numPageSizeRequested.Value); }
-		}
-		#endregion //PageSizeRequested
-
-		#region GridView
-		private Infragistics.Windows.DataPresenter.GridView GridView
-		{
-			get
-			{
-				if (null == this._gridView)
-					this._gridView = new Infragistics.Windows.DataPresenter.GridView();
-
-				return this._gridView;
-			}
-		}
-		#endregion //GridView
-
-		#region MaxCachedPages
-		private int MaxCachedPages
-		{
-			get { return Convert.ToInt32((double)this.numMaxCachedPages.Value); }
-		}
-		#endregion //MaxCachedPages
-
-		#region TreeView
-		private Infragistics.Windows.DataPresenter.TreeView TreeView
-		{
-			get
-			{
-				if (null == this._treeView)
-					this._treeView = new Infragistics.Windows.DataPresenter.TreeView();
-
-				return this._treeView;
-			}
-		}
-		#endregion //TreeView
-
-		#endregion //Private Properties
 
 		#region Private Methods
 
@@ -160,93 +53,6 @@ namespace DataPresenter.DataSources.OData.SampleApp
 		#endregion //Private Methods
 
 		#region Event Handlers
-
-		#region cboDataPresenterView_SelectionChanged
-		private void cboDataPresenterView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			if (null == this.dataPresenter1)
-				return;
-
-			string selectedView = (string)this.cboDataPresenterView.SelectedValue;
-			switch (selectedView)
-			{
-				case "CardView":
-					this.dataPresenter1.View = this.CardView;
-					break;
-				case "CarouselView":
-					this.dataPresenter1.View = this.CarouselView;
-					break;
-				case "GridView":
-					this.dataPresenter1.View = this.GridView;
-					break;
-				case "TreeView":
-					this.dataPresenter1.View = this.TreeView;
-					break;
-			}
-		}
-		#endregion //cboDataPresenterView_SelectionChanged
-
-		#region cboOdataSources_SelectionChanged
-		private void cboOdataSources_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			// Establish the DataSourceConfigurationInfo combobox item that was selected.  Refer to the MainWindow.xaml file
-			// to see how these combobox items were defined.
-			DataSourceConfigurationInfo dataSourceConfigInfo = this.cboOdataSources.SelectedItem as DataSourceConfigurationInfo;
-			if (null != dataSourceConfigInfo)
-			{
-				// If requested, null out the grid's DataSource before setting the new one.
-				if (this.chkNullOutDatasource.IsChecked.HasValue && this.chkNullOutDatasource.IsChecked.Value == true)
-					this.dataPresenter1.DataSource = null;
-
-				// Reset some FieldLayout related fields and settings.
-				this.dataPresenter1.FieldLayouts.Clear();
-				this.dataPresenter1.DefaultFieldLayout = null;
-				this.dataPresenter1.FieldLayoutSettings.AutoGenerateFields = true;
-
-				// For demo purposes, define a subset of fields to display in the grid.
-				//
-				// For the Northwind Orders table let's limit the number of fields by defining a DefaultFieldLayout
-				// in the XamDataPresenter that includes a subset of all the fields in the Orders table.  Not only will this
-				// limit the number of fields that are displayed by the grid, but it will also improve response time by
-				// limiting which columns of data are requested from the backend and transmitted over the connection.
-				if (dataSourceConfigInfo.BaseUri == @"http://services.odata.org/V4/Northwind/Northwind.svc" && dataSourceConfigInfo.EntitySet == "Orders")
-				{
-					this.dataPresenter1.FieldLayoutSettings.AutoGenerateFields = false;
-					FieldLayout fieldLayout = new FieldLayout();
-					fieldLayout.Fields.Add(new Field("CustomerID", typeof(string)));
-					fieldLayout.Fields.Add(new Field("EmployeeID", typeof(int)));
-					fieldLayout.Fields.Add(new Field("OrderDate", typeof(DateTime)));
-					fieldLayout.Fields.Add(new Field("ShipName", typeof(string)));
-					fieldLayout.Fields.Add(new Field("ShipAddress", typeof(string)));
-					fieldLayout.Fields.Add(new Field("ShipCity", typeof(string)));
-					fieldLayout.Fields.Add(new Field("ShipRegion", typeof(string)));
-					fieldLayout.Fields.Add(new Field("ShipPostalCode", typeof(string)));
-
-					this.dataPresenter1.FieldLayouts.Add(fieldLayout);
-					fieldLayout.IsDefault = true;
-				}
-
-
-				// ======================================================================================================
-				// Another way to limit the number of fields that are fetched from the backend to improve response time is to set 
-				// the DesiredFields property on the ODataDataSource. Refer to the MainWindow.xaml file to see how we have limited
-				// the number of fields requested and returned by the data source by setting the DesiredFields property of the 
-				// DataSourceConfigurationInfo combobox item.  We will use this configuration info below when we create the
-				// ODataDataSource instance.
-				// ======================================================================================================
-
-
-				// Set the grid's DataSource to an instance of an ODataDataSource created using the settings from the
-				// selected DataSourceConfigurationInfo.
-				this.dataPresenter1.DataSource = 
-					new ODataDataSource {	BaseUri = dataSourceConfigInfo.BaseUri,
-											EntitySet = dataSourceConfigInfo.EntitySet,
-											FieldsRequested = dataSourceConfigInfo.FieldsRequested,
-											PageSizeRequested = this.PageSizeRequested,
-											MaxCachedPages = this.MaxCachedPages };
-			}
-		}
-		#endregion //cboOdataSources_SelectionChanged
 
 		#region cboRecordFilterLogicalOperator_SelectionChanged
 		private void cboRecordFilterLogicalOperator_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -300,34 +106,6 @@ namespace DataPresenter.DataSources.OData.SampleApp
 		}
 		#endregion //colorPicker_SelectedColorChanged
 
-		#region numPageSizeRequested_EditModeEnded
-		private void numPageSizeRequested_EditModeEnded(object sender, Infragistics.Windows.Editors.Events.EditModeEndedEventArgs e)
-		{
-			if (null != this.CurrentDataSource)
-				this.CurrentDataSource.PageSizeRequested = this.PageSizeRequested;
-		}
-		#endregion //numPageSizeRequested_EditModeEnded
-
-		#region numMaxCachedPages_EditModeEnded
-		private void numMaxCachedPages_EditModeEnded(object sender, Infragistics.Windows.Editors.Events.EditModeEndedEventArgs e)
-		{
-			if (null != this.CurrentDataSource)
-				this.CurrentDataSource.MaxCachedPages = this.MaxCachedPages;
-		}
-		#endregion //numMaxCachedPages_EditModeEnded
-
 		#endregion //Event Handlers
 	}
-
-	#region DataSourceConfigurationInfo Class
-	public class DataSourceConfigurationInfo
-	{
-		public override string ToString() { return Description; }
-
-		public string BaseUri { get; set; }
-		public string Description { get; set; }
-		public string EntitySet{ get; set; }
-		public string[] FieldsRequested { get; set; }
-	}
-	#endregion //DataSourceConfigurationInfo Class
 }
---
+++ DataPresenter.DataSources.OData/DataPresenter.DataSources.OData.SampleApp/SampleViewModel.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataPresenter.DataSources.OData.SampleApp
+{
+	/// <summary>
+	/// A class that defines a ViewModel implementation for the OData Sample App.
+	/// </summary>
+	public class SampleViewModel : INotifyPropertyChanged
+	{
+		#region Member Variables
+		private int								_pageSizeRequested = 200;
+		private int								_maxCachedPages = 200;
+		private ODataDataSource					_currentDataSource;
+		private DataSourceConfigurationInfo		_currentDataSourceConfigurationInfo;
+		#endregion //Member Variables
+
+		#region Properties
+
+		#region CurrentDataSource
+		/// <summary>
+		/// Returns the current ODataDataSource based on the CurrentDataSourceConfigurationInfo property. (read only)
+		/// </summary>
+		public ODataDataSource CurrentDataSource
+		{
+			get
+			{
+				if (null == this._currentDataSource)
+					this.CurrentDataSource = new ODataDataSource();
+
+				return this._currentDataSource;
+			}
+			private set
+			{
+				if (this._currentDataSource != value)
+				{
+					this._currentDataSource = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+		#endregion //CurrentDataSource
+
+		#region CurrentDataSourceConfigurationInfo
+		/// <summary>
+		/// Returns/sets the DataSourceConfigurationInfo to use when creating the ODataDataSource that we 
+		/// expose via the CurrentDataSource property.
+		/// </summary>
+		public DataSourceConfigurationInfo CurrentDataSourceConfigurationInfo
+		{
+			get { return this._currentDataSourceConfigurationInfo; }
+			set
+			{
+				if (this._currentDataSourceConfigurationInfo != value)
+				{
+					this._currentDataSourceConfigurationInfo = value;
+					this.RaisePropertyChanged();
+
+					// ===============================================================================================================
+					// We could define a subset of fields to retrieve and display in the grid by defining a DefaultFieldLayout
+					// in the XamDataPresenter that includes a subset of all the fields in the Entity.  Not only will this
+					// limit the number of fields that are displayed by the grid, but it will also improve response time by
+					// limiting which columns of data are requested from the backend and transmitted over the connection.
+					// ===============================================================================================================
+
+					// ===============================================================================================================
+					// Another way to limit the number of fields that are fetched from the backend to improve response time is to set 
+					// the DesiredFields property on the ODataDataSource. Refer to the MainWindow.xaml file to see how we have limited
+					// the number of fields requested and returned by the data source by setting the DesiredFields property of the 
+					// DataSourceConfigurationInfo for the 'BuildingPermits' combobox item.  We will use this configuration info 
+					// below when we create the ODataDataSource instance.
+					// ===============================================================================================================
+
+
+					// Create and return an instance of an ODataDataSource using the settings from the
+					// current DataSourceConfigurationInfo.
+					this.CurrentDataSource =
+						new ODataDataSource
+						{
+							BaseUri				= this._currentDataSourceConfigurationInfo.BaseUri,
+							EntitySet			= this._currentDataSourceConfigurationInfo.EntitySet,
+							FieldsRequested		= this._currentDataSourceConfigurationInfo.FieldsRequested,
+							PageSizeRequested	= Convert.ToInt32(this.PageSizeRequested),
+							MaxCachedPages		= Convert.ToInt32(this.MaxCachedPages)
+						};
+				}
+			}
+		}
+		#endregion //CurrentDataSourceConfigurationInfo
+
+		#region MaxCachedPages
+		/// <summary>
+		/// Returns/sets the maximum number of fetched pages that the CurrentDataSource should cache.
+		/// </summary>
+		public int MaxCachedPages
+		{
+			get { return this._maxCachedPages; }
+			set
+			{
+				if (this._maxCachedPages!= value)
+				{
+					this._maxCachedPages= value;
+					this.RaisePropertyChanged();
+
+					this.CurrentDataSource.MaxCachedPages = value;
+				}
+			}
+		}
+		#endregion //MaxCachedPages
+
+		#region PageSizeRequested
+		/// <summary>
+		/// Returns/sets the logical page size (i.e., the number of records requested by each fetch request) that the CurrentDataSource 
+		/// should use when fetching data from a remote data source.
+		/// </summary>
+		public int PageSizeRequested
+		{
+			get { return this._pageSizeRequested; }
+			set
+			{
+				if (this._pageSizeRequested != value)
+				{
+					this._pageSizeRequested = value;
+					this.RaisePropertyChanged();
+
+					this.CurrentDataSource.PageSizeRequested = value;
+				}
+			}
+		}
+		#endregion //PageSizeRequested
+
+		#endregion //Properties
+
+		#region INotifyPropertyChanged Implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void RaisePropertyChanged([CallerMemberName] string caller = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(caller));
+		}
+		#endregion //INotifyPropertyChanged Implementation
+	}
+
+	#region DataSourceConfigurationInfo Class
+	/// <summary>
+	/// Class that contains configuration info used by the SampleViewModel to create an instance
+	/// of an ODataDataSource.
+	/// </summary>
+	public class DataSourceConfigurationInfo
+	{
+		/// <summary>
+		/// Returns a string representation of the instance.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString() { return Description; }
+
+		/// <summary>
+		/// Returns/sets the Uri of the OData service endpoint.
+		/// </summary>
+		public string BaseUri { get; set; }
+
+		/// <summary>
+		/// Returns/sets a description for the configuration information that can be displayed in the sample app UI.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Returns/sets the EntitySet (i.e., 'table') within the OData service endpoint defined by the BaseUri
+		/// from which data should be fetched.
+		/// </summary>
+		public string EntitySet { get; set; }
+
+		/// <summary>
+		/// Returns/sets an array of field names for which data should be fetched.  This field names specified must
+		/// exist in the schema for the desired EntitySet.
+		/// </summary>
+		public string[] FieldsRequested { get; set; }
+	}
+	#endregion //DataSourceConfigurationInfo Class
+}
